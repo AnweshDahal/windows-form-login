1deb604853d576083b45880f534cb8fc404f5201
AnweshDahal <anweshd2000@gmail.com>
2021-11-26 18:53:40 +0545

    Initialize form and change BG color

--- WindowsFormLogin/Form1.Designer.cs
+++ WindowsFormLogin/Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace WindowsFormLogin
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Salmon;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(183, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "User Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
